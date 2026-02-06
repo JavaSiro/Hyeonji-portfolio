84b626948a324bf72f6646705503aa673847c494
Javohir <javokhirsirojiddinov@gmail.com>
2026-02-06 14:19:21 +0900

    Fixed createClient ID

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,8 +1,8 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-02-06'
 
-// We are hardcoding these to ensure the build never fails
+// HARDCODED ID - This fixes the build error
 export const dataset = 'production'
-export const projectId = 'b7om90c3'
+export const projectId = 'b7om90c3' 
 
 export const useCdn = false
